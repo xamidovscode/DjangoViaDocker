899f5a89069b43734f1119d19c8c307fda964329
xamidovscode <xamidovcoder@gmail.com>
2025-06-20 16:55:16 +0500

    add a static file and gunicron

--- core/settings.py
+++ core/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Local folders to find static files during development
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
+# Destination folder where collectstatic puts everything (for production)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
